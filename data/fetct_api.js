d11e2ebfcbc8c1eaf10bd51a96b494b66509c893
Leng Bunvyrak <2024388leng@aupp.edu.kh>
2026-04-13 15:07:13 +0700

    api logic

--- data/fetct_api.js
+++ data/fetct_api.js
@@ -1,20 +1,48 @@
 const BASE_URL = "www.themealdb.com/api/json/v1"
 
-export const searchMeals = async (query) => {
-    const res = await fetch(`${BASE_URL}/search.php?s=${query}`)
-    return res.json()
+async function fetchData(endpoint) {
+    const res = await fetch(`${BASE_URL}${endpoint}`)
+    const data = await res.json()
+
+    return data
 }
 
-export const getMealByID = async (id) => {
-    const res = await fetch(`${BASE_URL}/lookup.php?i=${id}`)
-    return res.json()
+export function getMealByFirstLetter(letter) {
+    return fetchData(`/search.php?f=${letter}`)
 }
 
-export const getRandomMeal = async () => {
-    const res = await fetch(`${BASE_URL}/random.php`)
-    return res.json()
+export function getMealByID(id) {
+    return fetchData(`/lookup.php?i=${id}`)
 }
 
-export const filterCategory = async (query) => {
-    const res = await fetch(`${BASE_URL}/filter.php?c=${query}`)
+export function getRandomMeal() {
+    return fetchData(`/random.php`)
+}
+
+export function getCategories() {
+    return fetchData(`/categories.php`)
+}
+
+export function getCategoriesList() {
+    return fetchData(`/list.php?c=list`)
+}
+
+export function getAreaList() {
+    return fetchData(`/list.php?a=list`)
+}
+
+export function getIngredientList() {
+    return fetchData(`/list.php?i=list`)
+}
+
+export function filterByIngredient(ingredient) {
+    return fetchData(`/filter.php?c=${ingredient}`)
+}
+
+export function filterByCategory(category) {
+    return fetchData(`/filter.php?c=${category}`)
+}
+
+export function filterByArea(area) {
+    return fetchData(`/filter.php?c=${area}`)
 }
